ec74b87ae77fb4ca3d03e47bfaa2cf783efdc803
Chris-Th <muhmuhmiau@proton.me>
2022-12-21 11:13:40 +0100

    final

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 function goodFunction() {
+    
+    
+    
+    function bravo() {
+        let goodFb = document.getElementById('feedb').innerHTML = "bravo";
+        return goodFb;
+    }
+    function greenFont() {
+        let greenFb = document.getElementById('feedb').style.color = "green";
+        return greenFb;
+    }
 
+    function heartEyes() {
+        let hEyes = document.getElementById('feedb').className = "bi bi-emoji-heart-eyes myemo";
+
+        return hEyes;
+
+    }
+    
+    bravo();
+    greenFont();
+   // heartEyes();
+    
+    
+    
 }
 
 function badFunction() {
+    let badFb = document.getElementById('feedb').innerHTML = "Nooooooo!";
+    return badFb;
 
 }
